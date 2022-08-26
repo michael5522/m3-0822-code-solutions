1d556ac2a2b7a7b06d8bf5f3616c1922c205459f
Michael Chang <michaelchang.kuo@gmail.com>
2022-08-26 00:37:56 +0000

    finished es6-classes

---
+++ es6-classes/example-class.js
@@ -0,0 +1,26 @@
+// function ExampleClass() {
+
+// }
+
+class ExampleClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+const example = new ExampleClass();
+
+console.log('typeof example:', typeof example);
+console.log('value of example:', example);
+
+console.log('typeof example.__proto__', typeof example.__proto__);
+console.log('value of example.__proto__', example.__proto__);
+
+console.log('typeof ExampleClass.prototype:', typeof ExampleClass.prototype);
+console.log('value of ExampleClass.prototype:', ExampleClass.prototype);
+
+console.log('example instanceof ExampleClass', example instanceof ExampleClass);
