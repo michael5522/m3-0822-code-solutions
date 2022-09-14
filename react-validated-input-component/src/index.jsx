c1c1768ce016f29d8a6b396bf329982af2e82ed4
Michael Chang <michaelchang.kuo@gmail.com>
2022-09-14 02:29:42 +0000

    finished validated input

---
+++ react-validated-input-component/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ValidatedInput from './validated-input';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+const element = (
+  <ValidatedInput />
+);
+root.render(element);
