fed2ef18f8e03247d48151cff411e6b0965749fc
Michael Chang <michaelchang.kuo@gmail.com>
2022-10-04 23:54:51 +0000

    finished react hash routing

---
+++ react-hash-routing/client/app.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Header from './components/header';
+import Catalog from './pages/catalog';
+import NotFound from './pages/not-found';
+import ProductDetails from './pages/product-details';
+import { parseRoute } from './lib';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    /**
+     * Listen for hash change events on the window object
+     * Each time the window.location.hash changes, parse
+     * it with the parseRoute() function and update state
+     */
+    // showView(window.location.hash);
+    window.addEventListener('hashchange', () => {
+      const route = parseRoute(window.location.hash);
+      this.setState({ route });
+
+      // showView(window.location.hash);
+    });
+
+    // function showView(newHash) {
+
+    //   console.log('newHash:', newHash);
+    // }
+
+  }
+
+  renderPage() {
+    // console.log('111', this.state.route);
+    const { route } = this.state;
+    if (route.path === '') {
+      return <Catalog />;
+    }
+    if (route.path === 'products') {
+      const productId = route.params.get('productId');
+      // console.log('productID', productId);
+
+      return <ProductDetails productId={productId} />;
+    }
+    return <NotFound />;
+  }
+  // console.log('222', route);
+
+  render() {
+    return (
+      <>
+        <Header />
+        { this.renderPage() }
+      </>
+    );
+  }
+}
