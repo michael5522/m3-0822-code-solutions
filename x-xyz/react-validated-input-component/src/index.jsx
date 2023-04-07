90693f1b59c805c8ecf09f8b8dfd0b6d820dfb1c
Michael Chang <michaelchang.kuo@gmail.com>
2023-04-07 23:46:52 +0000

    validate input

---
+++ x-xyz/react-validated-input-component/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Validate from './validate';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<Validate />);
